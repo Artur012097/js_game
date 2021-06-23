2adabe50113cfe8a7b9101f1c7d725f1fd2ea7e8
artur012097 <artur012097.ax@gmail.com>
2021-06-24 00:30:25 +0400

    add test

---
+++ script/main.js
@@ -0,0 +1,15 @@
+$('#roomsSlider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    prevArrow: $('.rooms__prev'),
+    nextArrow: $('.rooms__next')
+  });
+
+  $('#feedbackSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.feedback__prev'),
+    nextArrow: $('.feedback__next')
+  });
