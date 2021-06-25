316c78c387ea3d6ec3217fb9ea39bb9679d4dae4
artur012097 <artur012097.ax@gmail.com>
2021-06-25 20:34:59 +0400

    some changes and adaptive

--- script/main.js
+++ script/main.js
@@ -3,7 +3,27 @@ $('#roomsSlider').slick({
     slidesToScroll: 4,
     arrows: true,
     prevArrow: $('.rooms__prev'),
-    nextArrow: $('.rooms__next')
+    nextArrow: $('.rooms__next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 568,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   $('#feedbackSlider').slick({
@@ -13,3 +33,11 @@ $('#roomsSlider').slick({
     prevArrow: $('.feedback__prev'),
     nextArrow: $('.feedback__next')
   });
+
+  let burger = document.getElementById('burger')
+  let menu = document.getElementById('mobileMenu')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('open')
+    menu.classList.toggle('open')
+  })
